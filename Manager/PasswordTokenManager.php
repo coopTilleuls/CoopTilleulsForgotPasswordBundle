174cf7a63e44e06763bc5772dff089cfd6ea7566
Amrouche Hamza <laharl.moon@gmail.com>
2017-01-16 10:01:59 +0100

    feat: update random lib for php 7.1 (#19)
    * feat: update random lib for php 7.1
    
    * fix deps
    
    * fix until randomlib is fixed
    
    * useless blank line

--- Manager/PasswordTokenManager.php
+++ Manager/PasswordTokenManager.php
@@ -55,10 +55,17 @@ class PasswordTokenManager
         /** @var AbstractPasswordToken $passwordToken */
         $passwordToken = new $this->passwordTokenClass();
 
-        $factory = new Factory();
-        $generator = $factory->getGenerator(new Strength(Strength::MEDIUM));
+        if (version_compare(phpversion(), '7.0', '>')) {
+            $passwordToken->setToken(random_bytes(50));
+        } else {
+            $factory = new Factory();
+            $generator = $factory->getGenerator(new Strength(Strength::MEDIUM));
 
-        $passwordToken->setToken($generator->generateString(50, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+            $passwordToken->setToken(
+                $generator->generateString(50, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+            );
+        }
+
         $passwordToken->setUser($user);
         $passwordToken->setExpiresAt($expiresAt ?: new \DateTime($this->defaultExpiresIn));
 
