64f6b0ce0ea327f4047d43507c92638b61f6a987
Vincent CHALAMON <vincentchalamon@gmail.com>
2019-01-08 12:03:21 +0100

    Fix routing deprecation (#49)

---
+++ tests/Controller/ResetPasswordTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the CoopTilleulsForgotPasswordBundle package.
+ *
+ * (c) Vincent Chalamon <vincent@les-tilleuls.coop>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\ForgotPasswordBundle\Controller;
+
+use CoopTilleuls\ForgotPasswordBundle\Controller\ResetPassword;
+use CoopTilleuls\ForgotPasswordBundle\Entity\AbstractPasswordToken;
+use CoopTilleuls\ForgotPasswordBundle\Manager\ForgotPasswordManager;
+use CoopTilleuls\ForgotPasswordBundle\Normalizer\NormalizerInterface;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Vincent Chalamon <vincentchalamon@gmail.com>
+ */
+final class ResetPasswordTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ForgotPasswordManager|ObjectProphecy
+     */
+    private $managerMock;
+
+    /**
+     * @var NormalizerInterface|ObjectProphecy
+     */
+    private $normalizerMock;
+
+    /**
+     * @var AbstractPasswordToken|ObjectProphecy
+     */
+    private $tokenMock;
+
+    protected function setUp()
+    {
+        $this->managerMock = $this->prophesize(ForgotPasswordManager::class);
+        $this->normalizerMock = $this->prophesize(NormalizerInterface::class);
+        $this->tokenMock = $this->prophesize(AbstractPasswordToken::class);
+    }
+
+    public function testResetPasswordAction()
+    {
+        $this->managerMock->resetPassword('email', 'foo@example.com')->shouldBeCalledTimes(1);
+        $controller = new ResetPassword($this->managerMock->reveal());
+        $response = $controller('email', 'foo@example.com');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals('', $response->getContent());
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+}
---
+++ tests/Controller/UpdatePasswordTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the CoopTilleulsForgotPasswordBundle package.
+ *
+ * (c) Vincent Chalamon <vincent@les-tilleuls.coop>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\ForgotPasswordBundle\Controller;
+
+use CoopTilleuls\ForgotPasswordBundle\Controller\UpdatePassword;
+use CoopTilleuls\ForgotPasswordBundle\Entity\AbstractPasswordToken;
+use CoopTilleuls\ForgotPasswordBundle\Manager\ForgotPasswordManager;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Vincent Chalamon <vincentchalamon@gmail.com>
+ */
+final class UpdatePasswordTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ForgotPasswordManager|ObjectProphecy
+     */
+    private $managerMock;
+
+    /**
+     * @var AbstractPasswordToken|ObjectProphecy
+     */
+    private $tokenMock;
+
+    protected function setUp()
+    {
+        $this->managerMock = $this->prophesize(ForgotPasswordManager::class);
+        $this->tokenMock = $this->prophesize(AbstractPasswordToken::class);
+    }
+
+    public function testUpdatePasswordAction()
+    {
+        $this->managerMock->updatePassword($this->tokenMock->reveal(), 'bar')->shouldBeCalledTimes(1);
+        $controller = new UpdatePassword($this->managerMock->reveal());
+        $response = $controller($this->tokenMock->reveal(), 'bar');
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals('', $response->getContent());
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+}
