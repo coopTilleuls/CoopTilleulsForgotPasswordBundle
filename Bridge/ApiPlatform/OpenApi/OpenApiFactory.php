aa187ff90dae31a88ee27990cbed75317dbba5e4
Alexis <contact@alexiscassan.fr>
2021-07-29 16:15:31 +0200

    Refactoring OpenApi decorator for api-platform

--- Bridge/ApiPlatform/OpenApi/OpenApiFactory.php
+++ Bridge/ApiPlatform/OpenApi/OpenApiFactory.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace CoopTilleuls\ForgotPasswordBundle\Bridge\ApiPlatform\OpenApi;
 
 use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
-use ApiPlatform\Core\OpenApi\Model\Components;
 use ApiPlatform\Core\OpenApi\Model\Operation;
 use ApiPlatform\Core\OpenApi\Model\PathItem;
 use ApiPlatform\Core\OpenApi\Model\RequestBody;
@@ -38,139 +37,144 @@ final class OpenApiFactory implements OpenApiFactoryInterface
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = ($this->decorated)($context);
-        $paths = $openApi->withTags(['name' => 'Forgot password'])->getPaths();
+        $schemas = $openApi->getComponents()->getSchemas();
 
-        // Add POST /forgot-password/ path
-        /** @var PathItem $path */
-        $path = $paths->getPath('/forgot-password/') ?: new PathItem();
-        $paths->addPath('/forgot-password/', $path->withPost(new Operation(
-            'postForgotPassword',
-            ['Forgot password'],
-            [
-                204 => [
-                    'description' => 'Valid email address, no matter if user exists or not',
-                ],
-                400 => [
-                    'description' => 'Missing email parameter or invalid format',
+        $schemas['ForgotPassword:reset'] = new \ArrayObject([
+            'type' => 'object',
+            'required' => ['password'],
+            'properties' => [
+                'password' => [
+                    'type' => 'string',
                 ],
             ],
-            'Generates a token and send email',
-            '',
+        ]);
+
+        $schemas['ForgotPassword:validate'] = new \ArrayObject([
+            'type' => 'object',
+        ]);
+
+        $schemas['ForgotPassword:request'] = new \ArrayObject([
+            'type' => 'object',
+            'required' => ['email'],
+            'properties' => [
+                'email' => [
+                    'type' => 'string',
+                ],
+            ],
+        ]);
+
+        $pathItem = new PathItem(
+            'ForgotPassword',
             null,
-            [],
-            new RequestBody('Request a new password', new \ArrayObject([
-                'application/json' => [
-                    'schema' => [
-                        '$ref' => '#/components/schemas/ForgotPassword:request',
+            null,
+            null,
+            null,
+            new Operation(
+                'postForgotPassword',
+                ['Forgot password'],
+                [
+                    204 => [
+                        'description' => 'Valid email address, no matter if user exists or not',
+                    ],
+                    400 => [
+                        'description' => 'Missing email parameter or invalid format',
                     ],
                 ],
-            ]))
-        )));
-        $openApi->withComponents(new Components(new \ArrayObject([
-            'ForgotPassword:request' => [
-                'type' => 'object',
-                'description' => '',
-                'required' => ['email'],
-                'properties' => [
-                    'email' => [
-                        'type' => 'string',
-                    ],
-                ],
-            ],
-        ])));
-
-        // Add GET /forgot-password/{token} path
-        /** @var PathItem $path */
-        $path = $paths->getPath('/forgot-password/{token}') ?: new PathItem();
-        $paths->addPath('/forgot-password/', $path->withGet(new Operation(
-            'getForgotPassword',
-            ['Forgot password'],
-            [
-                200 => [
-                    'description' => 'Authenticated user',
-                    'content' => [
+                'Generates a token and send email',
+                '',
+                null,
+                [],
+                new RequestBody(
+                    'Request a new password',
+                    new \ArrayObject([
                         'application/json' => [
                             'schema' => [
-                                '$ref' => '#/components/schemas/ForgotPassword:validate',
+                                '$ref' => '#/components/schemas/ForgotPassword:request',
+                            ],
+                        ],
+                    ])
+                )
+            )
+        );
+        $openApi->getPaths()->addPath('/forgot_password/', $pathItem);
+
+        $pathItem = new PathItem(
+            'ForgotPassword',
+            null,
+            '',
+            new Operation(
+                'getForgotPassword',
+                ['Forgot password'],
+                [
+                    200 => [
+                        'description' => 'Authenticated user',
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/ForgotPassword:validate',
+                                ],
                             ],
                         ],
                     ],
+                    404 => [
+                        'description' => 'Token not found or expired',
+                    ],
                 ],
-                404 => [
-                    'description' => 'Token not found or expired',
-                ],
-            ],
-            'Validates token',
-            '',
-            null,
-            [
+                'Validates token',
+                '',
+                null,
                 [
-                    'name' => 'token',
-                    'in' => 'path',
-                    'required' => true,
-                    'schema' => [
-                        'type' => 'string',
+                    [
+                        'name' => 'token',
+                        'in' => 'path',
+                        'required' => true,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
                     ],
-                ],
-            ]
-        )));
-        $openApi->withComponents(new Components(new \ArrayObject([
-            'ForgotPassword:validate' => [
-                'type' => 'object',
-                'description' => '',
-            ],
-        ])));
-
-        // Add POST /forgot-password/{token} path
-        /** @var PathItem $path */
-        $path = $paths->getPath('/forgot-password/{token}') ?: new PathItem();
-        $paths->addPath('/forgot-password/', $path->withPost(new Operation(
-            'postForgotPasswordToken',
-            ['Forgot password'],
-            [
-                204 => [
-                    'description' => 'Email address format valid, no matter if user exists or not',
-                ],
-                400 => [
-                    'description' => 'Missing password parameter',
-                ],
-                404 => [
-                    'description' => 'Token not found',
-                ],
-            ],
-            'Resets user password from token',
-            '',
+                ]
+            ),
             null,
-            [
+            new Operation(
+                'postForgotPasswordToken',
+                ['Forgot password'],
                 [
-                    'name' => 'token',
-                    'in' => 'path',
-                    'required' => true,
-                    'schema' => [
-                        'type' => 'string',
+                    204 => [
+                        'description' => 'Email address format valid, no matter if user exists or not',
+                    ],
+                    400 => [
+                        'description' => 'Missing password parameter',
+                    ],
+                    404 => [
+                        'description' => 'Token not found',
                     ],
                 ],
-            ],
-            new RequestBody('Reset password', new \ArrayObject([
-                'application/json' => [
-                    'schema' => [
-                        '$ref' => '#/components/schemas/ForgotPassword:reset',
+                'Resets user password from token',
+                '',
+                null,
+                [
+                    [
+                        'name' => 'token',
+                        'in' => 'path',
+                        'required' => true,
+                        'schema' => [
+                            'type' => 'string',
+                        ],
                     ],
                 ],
-            ]))
-        )));
-        $openApi->withComponents(new Components(new \ArrayObject([
-            'ForgotPassword:reset' => [
-                'type' => 'object',
-                'description' => '',
-                'required' => ['password'],
-                'properties' => [
-                    'password' => [
-                        'type' => 'string',
-                    ],
-                ],
-            ],
-        ])));
+                new RequestBody(
+                    'Reset password',
+                    new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                '$ref' => '#/components/schemas/ForgotPassword:reset',
+                            ],
+                        ],
+                    ])
+                )
+            )
+        );
+        $openApi->getPaths()->addPath('/forgot_password/{token}', $pathItem);
 
         return $openApi;
     }
