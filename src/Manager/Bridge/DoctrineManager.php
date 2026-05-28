461c742ac8d857266bc1a7c1ba0c58077d2de7c9
Vincent <407859+vincentchalamon@users.noreply.github.com>
2026-05-28 08:55:05 +0200

    fix: CS (#162)

--- src/Manager/Bridge/DoctrineManager.php
+++ src/Manager/Bridge/DoctrineManager.php
@@ -9,8 +9,6 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types=1);
-
 namespace CoopTilleuls\ForgotPasswordBundle\Manager\Bridge;
 
 use Doctrine\Common\Persistence\ManagerRegistry as LegacyManagerRegistry;
--- tests/app/bootstrap.php
+++ tests/app/bootstrap.php
@@ -9,8 +9,6 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types=1);
-
 use DG\BypassFinals;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
